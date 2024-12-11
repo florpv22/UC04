eb0619905a1c7def35e622ef1db6ef00ef37dfca
Flor Pizango <1812110123@untels.edu.pe>
2024-12-11 17:30:15 -0500

    segundo commit

--- app/src/main/java/com/example/dam_uc4_pizango/MainActivity.java
+++ app/src/main/java/com/example/dam_uc4_pizango/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.dam_uc4_pizango;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import android.view.View;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogin;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Definir el evento click sobre el botón Login
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+            }
+
         });
     }
 }
